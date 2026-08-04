c1ce8fd68c7145feb89324c303a6c14d3d084448
Soleman Kamal <raafkar935@gmail.com>
2026-08-04 14:13:46 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    base: "/balancely-dashboard/",
+
+    server: {
+        port: 3000,
     },
-  },
 
-  base: "/Balancely-ERP/",
-});
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});git add .
